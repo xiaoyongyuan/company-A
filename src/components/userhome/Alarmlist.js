b4894e4e76ad028a9e6caa2d855f312537eb3845
xiaoyongyuan <2207294253@qq.com>
2018-12-25 10:12:00 +0800

    报警

--- src/components/userhome/Alarmlist.js
+++ src/components/userhome/Alarmlist.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { DatePicker,Row,Col,Select,Button,Icon } from "antd";
+import { DatePicker,Row,Col,Select,Button,Icon,Modal } from "antd";
 import "../../style/ztt/css/police.css";
 import {post} from "../../axios/tools";
+import axios from "axios";
 import nodata from "../../style/imgs/nodata.png";
 const { RangePicker } = DatePicker;
 const Option = Select.Option;
@@ -9,11 +10,42 @@ class Alarmlist extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            type:0
+            type:0,
+            visible: false,
+            policeList:[]
         };
     }
+    showModal = () => {
+        this.setState({
+            visible: true,
+        });
+    }
+
+    handleOk = (e) => {
+        console.log(e);
+        this.setState({
+            visible: false,
+        });
+    }
+
+    handleCancel = (e) => {
+        console.log(e);
+        this.setState({
+            visible: false,
+        });
+    }
     componentDidMount() {
-        post({url:'/api/alarm/getlist'},(res)=>{
+        axios.get("ztt.json").then((res)=>{
+            console.log(res.data.data);
+            if(res.data.success){
+                this.setState({
+                    policeList:res.data.data
+                },()=>{
+                    console.log(this.state.policeList)
+                });
+            }
+        })
+       /* post({url:'/api/alarm/getlist'},(res)=>{
             if(res.success){
                 if(res.data>1){
                     this.setState({
@@ -25,7 +57,7 @@ class Alarmlist extends React.Component{
                     })
                 }
             }
-        })
+        })*/
     }
     //时间选择
     onChange = (date, dateString)=> {
@@ -57,11 +89,50 @@ class Alarmlist extends React.Component{
                         <Button type="primary">一键处理</Button>
                     </Col>
                 </Row>
-                <Row style={{marginTop:"70px",display:this.state.type==1?"none":"block"}}>
+
+                {/*<Row style={{marginTop:"70px",display:this.state.type==1?"none":"block"}}>
                     <Col style={{width:"100%",textAlign:"center"}}><div className="backImg"><img src={nodata} alt=""/></div></Col>
-                </Row>
-                <Row style={{display:this.state.type==1?"block":"none"}}>
-                    <Col xl={12} xxl={12} style={{marginTop:"40px"}}>
+                </Row>*/}
+                <Row style={{display:this.state.type==0?"block":"none"}}>
+                    {
+                        this.state.policeList.map((v,i)=>(
+                            <Col xl={12} xxl={12} style={{marginTop:"40px"}} key={i}>
+                                <Row>
+                                    <Col xl={3} xxl={2}>
+                                        <div className="handle">
+                                            <div className={v.static?"stateBackground":""}>{v.state}</div>
+                                        </div>
+                                    </Col>
+                                    <Col xl={9} xxl={7} className="policeIcon">
+                                        <div className="pliceImg">
+                                            <div className="img"></div>
+                                        </div>
+                                        <div className="camera" onClick={this.showModal}><Icon type="video-camera" theme="filled" /></div>
+                                    </Col>
+                                    <Col xl={11} xxl={12}>
+                                        <div className="policeContext">
+                                            <div className="triangle"></div>
+                                            <Row className="line-police">
+                                                <Col xl={12} xxl={12} className="policeName">{v.name}</Col>
+                                                <Col xl={12} xxl={12}>{v.police}</Col>
+                                            </Row>
+                                            <Row className="line-police">
+                                                <Col xl={12} xxl={12}>{v.times}</Col>
+                                                <Col xl={12} xxl={12}>报警对象：{v.policeObject}</Col>
+                                            </Row>
+                                            <Row className="line-police">
+                                                <Col xl={8} xxl={8}><Icon type="redo" />确认</Col>
+                                                <Col xl={8} xxl={8}><Icon type="redo" />虚报</Col>
+                                                <Col xl={8} xxl={8}><Icon type="redo" />忽略</Col>
+                                            </Row>
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        ))
+                    }
+
+                   {/* <Col xl={12} xxl={12} style={{marginTop:"40px"}}>
                         <Row>
                             <Col xl={3} xxl={2}>
                                 <div className="handle">
@@ -93,41 +164,18 @@ class Alarmlist extends React.Component{
                                 </div>
                             </Col>
                         </Row>
-                    </Col>
-                    <Col xl={12} xxl={12} style={{marginTop:"40px"}}>
-                        <Row>
-                            <Col xl={3} xxl={2}>
-                                <div className="handle">
-                                    <div className="handle-right">待处理</div>
-                                </div>
-                            </Col>
-                            <Col xl={9} xxl={7} className="policeIcon">
-                                <div className="pliceImg">
-                                    <div className="img"></div>
-                                </div>
-                                <div className="camera"><Icon type="video-camera" theme="filled" /></div>
-                            </Col>
-                            <Col xl={11} xxl={12}>
-                                <div className="policeContext">
-                                    <div className="triangle"></div>
-                                    <Row className="line-police">
-                                        <Col xl={12} xxl={12} className="policeName">理工大北门</Col>
-                                        <Col xl={12} xxl={12}>入侵报警</Col>
-                                    </Row>
-                                    <Row className="line-police">
-                                        <Col xl={12} xxl={12}>2018/12/09</Col>
-                                        <Col xl={12} xxl={12}>报警对象：无</Col>
-                                    </Row>
-                                    <Row className="line-police">
-                                        <Col xl={8} xxl={8}><Icon type="redo" />确认</Col>
-                                        <Col xl={8} xxl={8}><Icon type="redo" />虚报</Col>
-                                        <Col xl={8} xxl={8}><Icon type="redo" />忽略</Col>
-                                    </Row>
-                                </div>
-                            </Col>
-                        </Row>
-                    </Col>
+                    </Col>*/}
                 </Row>
+                <Modal
+                    title="Basic Modal"
+                    visible={this.state.visible}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}
+                >
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                </Modal>
             </div>
         )
     }
