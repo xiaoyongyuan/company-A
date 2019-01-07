5153b5d4b25701e6b0a5624930f625122e6268a8
shgg <547537959@qq.com>
2019-01-07 18:42:52 +0800

    .

---
+++ src/components/rollcall/RollcallHostoryModel.js
@@ -0,0 +1,110 @@
+import React,{Component} from "react";
+import {Row,Col} from "antd";
+import {post} from "../../axios/tools";
+import "../../style/ztt/css/rollCall.css";
+
+const rollset=[
+    {
+        code:89,
+        rrpic:'http://pic01.aokecloud.cn/alarm/1000011/pic/20181229//1000011_20181229100320.jpg',
+        rfinal:0,
+        cid:1000007,
+        rname:"理工大",
+        ifeveryday:"1",
+        resultdate:"2019-01-05 16:31:00",
+    
+     },
+     {
+        code:90,
+        rrpic:'http://pic01.aokecloud.cn/alarm/1000011/pic/20181229//1000011_20181229100320.jpg',
+        rfinal:1,
+        cid:1000007,
+        rname:"理工大",
+        ifeveryday:"0",
+        resultdate:"2019-01-05 16:31:00",
+    },
+    
+    {
+        code:91,
+        rrpic:'http://pic01.aokecloud.cn/alarm/1000011/pic/20181229//1000011_20181229094947.jpg',
+        rfinal:0,
+        cid:1000007,
+        rname:"理工大",
+        ifeveryday:"1",
+        resultdate:"2019-01-05 12:31:00",
+     },
+    
+     {
+        code:92,
+        rrpic:'http://pic01.aokecloud.cn/alarm/1000011/pic/20181229//1000011_20181229094947.jpg',
+        rfinal:1,
+        cid:1000007,
+        rname:"理工大",
+        ifeveryday:"0",
+        resultdate:"2019-01-05 12:31:00",
+    }
+    ,{
+        code:93,
+        rrpic:'http://pic01.aokecloud.cn/alarm/1000011/pic/20181229//1000011_20181229100320.jpg',
+        rfinal:2,
+        cid:1000007,
+        rname:"理工大",
+        ifeveryday:"1",
+        resultdate:"2019-01-05 11:07",
+     },
+     {
+        code:94,
+        rrpic:'http://pic01.aokecloud.cn/alarm/1000011/pic/20181229//1000011_20181229100320.jpg',
+        rfinal:0,
+        cid:1000007,
+        rname:"理工大",
+        ifeveryday:"0",
+        resultdate:"2019-01-05 11:07",
+    }
+];
+
+class RollcallHostoryModel extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+
+        };
+    }
+    
+    componentWillMount() {
+
+    }
+
+    componentDidMount() {
+        // post({url:"/api/rollcalldetail/getone",data:{code:this.props.code}},(res)=>{
+        //     if(res.success){
+
+        //     }
+        // })
+    }
+    normal =(status)=>{
+        if(status==0){
+            return "fontColor rollcallModelTitle";
+        }else if(status==1){
+            return "fontColor1 rollcallModelTitle";
+        }
+    };
+    render(){
+        return(
+            <div className="rollcallRecordModel">
+                <Row><Col span={24} className="rollcallModelTitle fontSizeModel">{rollset[this.props.code].rname} - {rollset[this.props.code].cid}</Col></Row>
+                <Row className="rollcallModel">
+                    <Col span={24}>
+                        <img src={rollset[this.props.code].rrpic} alt="" width="100%"/>
+                    </Col>
+                </Row>
+                <Row className="rollcallModel">
+                    <Col span={8} className="rollcallModelTitle">{rollset[this.props.code].resultdate}</Col>
+                    <Col span={8} className="rollcallModelTitle">{rollset[this.props.code].ifeveryday==0?"自动点名":"手动点名"}</Col>
+                    <Col span={8} className={this.normal(rollset[this.props.code].rfinal)}>{rollset[this.props.code].rfinal==0?"正常":"报警"}</Col>
+                </Row>
+            </div>
+        )
+    }
+}
+export default RollcallHostoryModel
