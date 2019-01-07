496a760bb93bb531ca783958c9b57298f11910ac
azj7319 <568963700@qq.com>
2019-01-07 14:01:57 +0800

    新增管理员设备页面

---
+++ src/components/adminequipment/index.js
@@ -0,0 +1,17 @@
+import React, { Component} from 'react';
+import {post} from "../../axios/tools";
+
+class AdminEquipment extends Component {
+    componentDidMount() {
+    }
+    render() {
+    
+        return (
+            <div className="AdminEquipment">
+                ddd
+            </div>
+        )
+    }
+}
+
+export default AdminEquipment;
