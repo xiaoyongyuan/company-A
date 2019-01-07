7aa6231cbd7025de2c12fed71358e7506824a0ea
shgg <547537959@qq.com>
2019-01-07 17:38:04 +0800

    .

--- src/components/rollcall/RollcallHostory.js
+++ src/components/rollcall/RollcallHostory.js
@@ -308,7 +308,7 @@ class RollcallHostory extends React.Component{
                 </div>
               
                 <div>
-                    <BackTop />
+                    <BackTop target ={()=>document.getElementById('scorll')} />
                 </div>
 
                 {/* <div onClick={this.BackTop } className="backtop">
