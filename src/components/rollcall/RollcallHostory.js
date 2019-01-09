29aa86d08e176bf5251aa1fe900243797663d011
shgg <547537959@qq.com>
2019-01-09 18:47:53 +0800

    。

--- src/components/rollcall/RollcallHostory.js
+++ src/components/rollcall/RollcallHostory.js
@@ -1,5 +1,5 @@
 import React, { Component} from 'react';
-import {Row, Col, Button, DatePicker, LocaleProvider, Timeline , Form,Modal } from "antd";
+import {Row, Col, Button, DatePicker, LocaleProvider, Timeline , Form,Modal,Spin } from "antd";
 import {post} from "../../axios/tools";
 import zh_CN from 'antd/lib/locale-provider/zh_CN';
 import '../../style/sjg/home.css';
@@ -163,6 +163,7 @@ class RollcallHostory extends React.Component{
             rollCallType:false,
             list:[],
             dataitem:{},
+            loading:false,
         }
     }
        
@@ -199,6 +200,8 @@ class RollcallHostory extends React.Component{
                 }
                 post({url:'/api/rollcalldetail/getlist_info_dayly',data:da},(res)=>{
                     console.log(res);
+                      
+
                     if(res.success){
                         const list=_this.state.list;
                         list.push(dataitem);
