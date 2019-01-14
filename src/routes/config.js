d32d2bac85ecc8256feb5ba68551aee73b1d2a61
azj7319 <568963700@qq.com>
2019-01-14 09:36:28 +0800

    删除无用代码

--- src/routes/config.js
+++ src/routes/config.js
@@ -32,7 +32,6 @@ export default {
             ],
         },
         { key: '/app/live/index', identi:['comp','user'], title: '直播', icon: 'camera', funct:'basic', component: 'Live' },
-        { key: '/app/adminequipment/index', identi:['comp','user'], title: '设备信息', icon: 'camera', funct:'basic', component: 'AdminEquipment' },
 
     ],
     // 非菜单相关路由
