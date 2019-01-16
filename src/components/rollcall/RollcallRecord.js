3e9020c0729b3e028872a926509f8b75241b09dd
yangalong <172778564@qq.com>
2019-01-16 16:19:05 +0800

    点名记录

--- src/components/rollcall/RollcallRecord.js
+++ src/components/rollcall/RollcallRecord.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Row, Col, Form, DatePicker, LocaleProvider, Input, Select,Modal} from "antd";
+import {Row, Col, Form, DatePicker, LocaleProvider, Input, Select,Modal,Pagination} from "antd";
 import moment from 'moment';
 import RollcallRecordModel from "./RollcallRecordModel";
 import zh_CN from "antd/lib/locale-provider/zh_CN";
@@ -9,56 +9,14 @@ import Button from "antd/es/button/button";
 import {post} from "../../axios/tools";
 const RangePicker = DatePicker.RangePicker;
 const Option = Select.Option;
-const rollset=[
-    {
-        code:0,
-        rname:"神道西侧",
-        cid:"18石马",
-        ifeveryday:"0",
-        resultdate:"2019-01-10 11:07:09",
-        rrpic:"http://pic01.aokecloud.cn/alarm/1000021/background/EFGABC018.jpg",
-        rfinal:"0"
-    },{
-        code:1,
-        rname:"神道入口",
-        cid:"16狮子",
-        ifeveryday:"1",
-        resultdate:"2019-01-04 11:06::09",
-        rrpic:"http://pic01.aokecloud.cn/alarm/1000021/background/EFGABC016.jpg",
-        rfinal:"1"
-    },{
-        code:2,
-        rname:"神道东侧",
-        cid:"17石像",
-        ifeveryday:"0",
-        resultdate:"2018-12-03 11:07",
-        rrpic:"http://pic01.aokecloud.cn/alarm/1000011/pic/20181229//1000011_20181229100320.jpg",
-        rfinal:"0"
-    },{
-        code:3,
-        rname:"中兴通讯",
-        cid:"tftt06",
-        ifeveryday:"1",
-        resultdate:"2019-01-02 11:07",
-        rrpic:"http://pic01.aokecloud.cn/alarm/1000004/pic/20190104//1000004_20190104172947.jpg",
-        rfinal:"1"
-    },{
-        code:4,
-        rname:"中兴通讯",
-        cid:"tftt06",
-        ifeveryday:"1",
-        resultdate:"2019-01-02 11:07",
-        rrpic:"http://pic01.aokecloud.cn/alarm/1000011/pic/20181229//1000011_20181229100320.jpg",
-        rfinal:"0"
-    }
-];
 class RollcallRecord extends React.Component{
     constructor(props){
         super(props);
         this.state={
             rollCall:[],
             rollsetList:[],
-            rollCallType:false
+            rollCallType:false,
+            page:1, //当前页数
         };
     }
     //对象名称
@@ -68,20 +26,25 @@ class RollcallRecord extends React.Component{
         })
     };
     //日期
-    onChangeDate = (dates, dateStrings)=> {
+    onChange = (date, dateString)=> {
         this.setState({
-            bdate:dates[0].format('YYYY-MM-DD HH:00:00'),
-            edate:dateStrings[1]
+            bdate:dateString[0],
+            edate:dateString[1]
         });
-
-    };
+        console.log(date, dateString);
+        console.log(dateString[0]);
+        console.log(dateString[1]);
+    }
     //model open
-    handlerollCallType =(index)=>{
-        console.log(index,"3333");
+    handlerollCallType =(code)=>{//打开弹层
+        console.log(code,"3333");
         this.setState({
             rollCallType:true,
-            code:index
-        })
+            code:code
+        },()=>{
+            console.log('code',code)
+            }
+        )
     };
     //model close
     handlerollClose =()=>{
@@ -107,35 +70,54 @@ class RollcallRecord extends React.Component{
         post({url:"/api/camera/getlist"},(res)=>{
             if(res.success){
                 this.setState({
-                    rollCall:res.data,
+                    rollCall:res.data
                 })
             }
         })
     };
     //点名列表
-    handleRollCallList =()=>{
-        post({url:"/api/rollcalldetail/getlist"},(res)=>{
+    handleRollCallList =(params={pagesize:3,pageindex:this.state.page,bdate:this.state.bdate,edate:this.state.edate,cid:this.state.cid,rname:this.state.calInput,})=>{
+        post({url:"/api/rollcalldetail/getlist",data:params},(res)=>{
             if(res.success){
                 this.setState({
-                    // rollsetList:rollset
-                    rollsetList:res.data
+                    rollsetList:res.data,
+                    totalcount:res.totalcount
                 })
             }
         });
 
     };
+    handleMenuClick = ()=>{
+        this.setState({
+            // cname:e.target.value,
+            page:1
+        });
+    }
     //查询
     handleSubmit =()=>{
+
         let datas={
             bdate:this.state.bdate,
             edate:this.state.edate,
             cid:this.state.cid,
-            rname:this.state.calInput
+            rname:this.state.calInput,
+            pagesize:3,
+            pageindex:this.state.page
         };
-        post({url:"/api/rollcalldetail/getlist",data:datas},()=>{
+
+        post({url:"/api/rollcalldetail/getlist",data:datas},(res)=>{
            this.setState({
-               rollsetList:rollset
+               rollsetList:res.data,
+               totalcount:res.totalcount
            })
+        })
+    };
+    hanlePageSize = (page) => { //翻页
+        console.log("page",page);
+        this.setState({
+            page:page
+        },()=>{
+            this.componentDidMount()
         })
     };
     componentDidMount() {
@@ -153,11 +135,9 @@ class RollcallRecord extends React.Component{
                                 label="日期"
                             >
                                 {getFieldDecorator('range-picker1')(
-                                    <RangePicker
-                                        ranges={{ Today: [moment(), moment()], 'This Month': [moment().startOf('month'), moment().endOf('month')] }}
-                                        showTime
-                                        format="YYYY/MM/DD HH:00:00"
-                                        onChange={this.onChangeDate}
+                                    <RangePicker onChange={this.onChange}
+                                                 showTime
+                                                 format="YYYY-MM-DD HH:mm:ss"
                                     />
                                 )}
                             </Form.Item>
@@ -189,6 +169,7 @@ class RollcallRecord extends React.Component{
                                 <Button
                                     type="primary"
                                     htmlType="submit"
+                                    onClick={this.handleMenuClick}
                                 >
                                    查询
                                 </Button>
@@ -196,19 +177,22 @@ class RollcallRecord extends React.Component{
                         </Form>
                     </div>
                 </LocaleProvider>
-                <Row>
+                <Row type="flex" justify="space-between">
                     {
                         this.state.rollsetList.map((v,i)=>(
                             <Col key={i} xs={12} sm={12} md={12} lg={12} xl={12} xxl={7} style={{marginTop:"30px"}}>
                                 <Row>
                                     <Col xs={12} sm={12} md={11} lg={11} xl={12} xxl={12}>
-                                        <img src={v.rrpic} alt="" width="100%" onClick={()=>this.handlerollCallType(i)} />
+                                        <img src={v.rrpic} alt="" width="100%" onClick={()=>this.handlerollCallType(v.code)} />
                                     </Col>
                                     <Col xs={12} sm={12} md={12} lg={12} xl={11} xxl={11} className="rollRow">
                                         <Row className="rollCall">{v.cameraname}-{v.rname}</Row>
                                         <Row className="rollCall">{v.ifeveryday==0?"自动点名":"手动点名"}</Row>
                                         <Row className="rollCall">
-                                            <Col className="overflow">{v.resultdate} <span className={this.normal(v.rfinal)}>{v.rfinal==0?"正常":"报警"}</span></Col>
+                                            <Col className="overflow">
+                                                {v.resultdate}
+                                                {v.rfinal==1?<span style={{color:'green'}}>正常</span>:<span style={{color:'red'}}>报警</span>}
+                                            </Col>
                                         </Row>
                                     </Col>
                                 </Row>
@@ -216,6 +200,7 @@ class RollcallRecord extends React.Component{
                         ))
                     }
                 </Row>
+                <Pagination defaultPageSize={3} current={this.state.page} total={this.state.totalcount}  onChange={this.hanlePageSize} className="pageSize"  />
                 <Modal
                 width={700}
                 title="点名记录详情"
