7cdff625cd28947b18c823fdf49e94d6fd7fe1fa
azj7319 <568963700@qq.com>
2019-01-18 17:16:47 +0800

    历史

---
+++ src/components/patrol/PatrolHostory.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+class PatrolHostory extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+        };
+    }
+
+    componentDidMount() {
+    }
+
+    render(){
+        return(
+            <div className="PatrolHostory">
+                
+            </div>
+        )
+    }
+}
+export default PatrolHostory;
