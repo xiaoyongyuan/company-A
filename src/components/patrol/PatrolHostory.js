e3adb5e462d6d27a4a7c9b189ed2a747a17b4bc3
shgg <547537959@qq.com>
2019-01-22 10:22:22 +0800

    .

--- src/components/patrol/PatrolHostory.js
+++ src/components/patrol/PatrolHostory.js
@@ -179,7 +179,7 @@ class RollcallHostory extends React.Component{
                                         onOpenChange={this.handleStartOpenChange}
                                     />
                                 )}
-                                
+
                             </Form.Item>
                             </Col>
                             <Col xl={4} xxl={3} lg={6}>
@@ -221,12 +221,11 @@ class RollcallHostory extends React.Component{
                                                     <span>共点名 {item.totalcount}个对象</span>,
                                                     {item.status===0? <span style={{padding:"0 0 0 14px"}}>执行中...</span>:
                                                         <span>
-                                                            <span style={item.status==1?{display:'none'}:{display:'inline-block'}}>
-                                                                <span>有 {item.unhandle}个对象未巡更，</span>
-                                                                <span style={item.status==2?{display:'none'}:{display:'inline-block'}}>有{item.handling}个对象正在处理中,</span>
-                                                            </span>
-                                                            <span style={item.status==2?{display:'none'}:{display:'inline-block'}}>有 {item.handle_true}个对象巡更正常,有{item.handle_false}个对象巡更异常，</span>
-                                                       </span>
+                                                            {item.status===1?<span>已完成有<span>{item.handle_true}个对象巡更正常,有{item.handle_false}个对象巡更异常，
+                                                            </span> </span>: "" }
+                                                            {item.status===2?<span>有 {item.unhandle}个对象未巡更，有 {item.handle_true}个对象巡更正常,有{item.handle_false}个对象巡更异常， 
+                                                            </span>: ""}
+                                                        </span>
                                                     }
                                                 </div>
                                             </div>
