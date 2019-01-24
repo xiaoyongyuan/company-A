be82c6f511d5e780830bd8966f21d932ef03e122
yangalong <172778564@qq.com>
2019-01-24 17:00:48 +0800

    PatrolRecordModel修改代码不规范

--- src/components/patrol/PatrolRecordModel.js
+++ src/components/patrol/PatrolRecordModel.js
@@ -24,7 +24,7 @@ class PatrolRecordModel extends Component{
 
     }
     componentWillReceiveProps(nextProps){
-        if( nextProps.visible != vis){
+        if( nextProps.visible !== vis){
             vis=nextProps.visible;
             if(nextProps.visible){
                 vis=nextProps.visible;
@@ -57,10 +57,10 @@ class PatrolRecordModel extends Component{
                    <Col span={7} offset={3}>{this.state.paList.pteam+' ('+this.state.paList.pbdate+':00:00 —— '+this.state.paList.pedate+':00:00)'}</Col>
                </Row>
                <Row>
-                   <Col span={24}><img src={this.state.paList.ppic} alt="nodata" width="100%"/></Col>
+                   <Col span={24}><img src={this.state.paList.ppic} alt="nodata" width="100%" /></Col>
                </Row>
                <Row style={{margin:"10px 0px"}}>
-                   <Col span={8}>处理结果: {this.state.paList.phandle==1?<span style={{color:'#0f0'}}>通过</span>:<span style={{color:'#f00'}}>不通过</span>}</Col>
+                   <Col span={8}>处理结果: {this.state.paList.phandle===1?<span style={{color:'#0f0'}}>通过</span>:<span style={{color:'#f00'}}>不通过</span>}</Col>
                   <Col span={8}>处理人: {this.state.paList.handlename}</Col>
                   <Col span={8}>处理时间: {this.state.paList.phdate}</Col>
                </Row>
