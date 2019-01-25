bd82ec929ca2333407a3013afdc9e61d1c05c4d1
yangalong <172778564@qq.com>
2019-01-25 17:31:15 +0800

    自定义地图

--- src/style/ztt/map/scenemap.js
+++ src/style/ztt/map/scenemap.js
@@ -1,320 +1,69 @@
 var scenemap={
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "properties": {
-        "stroke": "#94c56c",
-        "stroke-width": 2,
-        "stroke-opacity": 1,
-        "fill": "#94c56c",
-        "fill-opacity": 0.5
-      },
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [
-          [
-            [
-              109.00986671447752,
-              34.15482230005668
-            ],
-            [
-              109.02626037597656,
-              34.15482230005668
-            ],
-            [
-              109.02692556381226,
-              34.181364436172096
-            ],
-            [
-              109.00978088378908,
-              34.18134668502452
-            ],
-            [
-              109.00986671447752,
-              34.15482230005668
-            ]
-          ]
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "stroke": "#9b8e7e",
-        "stroke-width": 2,
-        "stroke-opacity": 1,
-        "fill": "#9b8e7e",
-        "fill-opacity": 0.5
-      },
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [
-          [
-            [
-              109.01407241821295,
-              34.177689868994314
-            ],
-            [
-              109.02235507965092,
-              34.177689868994314
-            ],
-            [
-              109.02235507965092,
-              34.179607054444645
-            ],
-            [
-              109.01407241821295,
-              34.179607054444645
-            ],
-            [
-              109.01407241821295,
-              34.177689868994314
-            ]
-          ]
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "marker-color": "#9b8e7e",
-        "marker-size": "medium",
-        "marker-symbol": "ferry"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          109.02390003204346,
-          34.17467198883092
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "marker-color": "#9b8e7e",
-        "marker-size": "medium",
-        "marker-symbol": "ferry"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          109.02390003204346,
-          34.17229311310708
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "marker-color": "#9b8e7e",
-        "marker-size": "medium",
-        "marker-symbol": "ferry"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          109.02390003204346,
-          34.16998518451599
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "marker-color": "#9b8e7e",
-        "marker-size": "medium",
-        "marker-symbol": "ferry"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          109.02390003204346,
-          34.16760617668499
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "marker-color": "#9b8e7e",
-        "marker-size": "medium",
-        "marker-symbol": "ferry"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          109.0239429473877,
-          34.16526261087611
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "marker-color": "#9b8e7e",
-        "marker-size": "medium",
-        "marker-symbol": "ferry"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          109.0239429473877,
-          34.16295449004889
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "marker-color": "#9b8e7e",
-        "marker-size": "medium",
-        "marker-symbol": "ferry"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          109.0239429473877,
-          34.160646306117926
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "marker-color": "#9b8e7e",
-        "marker-size": "medium",
-        "marker-symbol": "ferry"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          109.0239429473877,
-          34.158302547099595
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "marker-color": "#9b8e7e",
-        "marker-size": "medium",
-        "marker-symbol": "ferry"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          109.0239429473877,
-          34.15599423599647
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "marker-color": "#9b8e7e",
-        "marker-size": "medium",
-        "marker-symbol": "ferry"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          109.01188373565674,
-          34.17463648371606
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "marker-color": "#9b8e7e",
-        "marker-size": "medium",
-        "marker-symbol": "ferry"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          109.01188373565674,
-          34.17002069158771
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "marker-color": "#9b8e7e",
-        "marker-size": "medium",
-        "marker-symbol": "ferry"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          109.01188373565674,
-          34.167677192815034
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "marker-color": "#9b8e7e",
-        "marker-size": "medium",
-        "marker-symbol": "ferry"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          109.01192665100098,
-          34.1653691380062
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "marker-color": "#9b8e7e",
-        "marker-size": "medium",
-        "marker-symbol": "ferry"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          109.01192665100098,
-          34.16306102009149
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "marker-color": "#9b8e7e",
-        "marker-size": "medium",
-        "marker-symbol": "ferry"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          109.01192665100098,
-          34.16071732810289
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "marker-color": "#9b8e7e",
-        "marker-size": "medium",
-        "marker-symbol": "ferry"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          109.01201248168945,
-          34.156136287732515
-        ]
-      }
-    }
-  ]
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        [
+                            111.07177734375,
+                            39.554883059924016
+                        ],
+                        [
+                            108.5888671875,
+                            37.85750715625203
+                        ],
+                        [
+                            107.02880859375,
+                            37.59682400108367
+                        ],
+                        [
+                            107.77587890625,
+                            36.65079252503471
+                        ],
+                        [
+                            108.65478515625,
+                            35.40696093270201
+                        ],
+                        [
+                            107.3583984375,
+                            34.97600151317591
+                        ],
+                        [
+                            106.67724609375,
+                            34.03445260967645
+                        ],
+                        [
+                            105.18310546875,
+                            32.54681317351514
+                        ],
+                        [
+                            108.017578125,
+                            31.2221970321032
+                        ],
+                        [
+                            110.85205078124999,
+                            31.59725256170666
+                        ],
+                        [
+                            112.6318359375,
+                            32.36140331527543
+                        ],
+                        [
+                            112.30224609374999,
+                            34.88593094075317
+                        ],
+                        [
+                            111.07177734375,
+                            39.554883059924016
+                        ]
+                    ]
+                ]
+            }
+        }
+    ]
 }
 export default scenemap;
