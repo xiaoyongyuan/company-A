2de24cfae49dfbe818d97686e31fb521fac93a66
yangalong <172778564@qq.com>
2019-01-28 14:53:55 +0800

    index.js修改代码报警

--- src/components/live/index.js
+++ src/components/live/index.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import videojs from 'video.js';
 import 'video.js/dist/video-js.min.css';
-import videoswf from '../../utils/video/video-js.swf';
 
 export default class Live extends React.Component {
   componentDidMount() {
     // videojs.options.flash.swf = videoswf;
     this.player = videojs('myvideo', {
-        preload: 'auto',  // 预加载
-        bigPlayButton: {},  // 大按钮
-        autoplay: true,   // 自动播放
-        controls: true,  // 是否开启控制栏
-        width: 800,   // 播放器宽度
-        height: 600,  // 播放器高度
+        preload: 'auto', // 预加载
+        bigPlayButton: {}, // 大按钮
+        controls: true, // 是否开启控制栏
+        width: 800, // 播放器宽度
+        height: 600, // 播放器高度
         playbackRates: [1, 1.5, 2], 
         muted: true, //是否循环播放
         loop : true, //是否静音
-        autoplay:true, //是否自动播放     
+        autoplay:true, //是否自动播放
     }, function onPlayerReady() {
       this.src({
         src: 'rtmp://58.200.131.2:1935/livetv/hunantv',
@@ -37,7 +35,7 @@ export default class Live extends React.Component {
     return (
       <div>    
         <div data-vjs-player>
-          <video ref={ node => this.videoNode = node } className="video-js" id="myvideo"></video>
+          <video ref={ node => this.videoNode = node } className="video-js" id="myvideo" />
         </div>
       </div>
     )
