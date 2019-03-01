cb5903995063ea2afb6e59b54f2169e5b20126e4
azj7319 <568963700@qq.com>
2019-03-01 18:32:04 +0800

    场景图

--- src/components/companyhome/Scenedata.js
+++ src/components/companyhome/Scenedata.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import ReactEcharts from 'echarts-for-react';
 import echarts from 'echarts';
-import scenemap from "../../style/ztt/map/scenemap";
-import xianmap from "../../style/ztt/map/xianmap";
+import apg from "../../style/ztt/map/1000020.json";
+import mqwl from "../../style/ztt/map/1000022.json";
+
+import axios from 'axios';
+
 
 class Scenedata extends Component {
     constructor(props){
@@ -20,11 +23,23 @@ class Scenedata extends Component {
     }
 
     componentDidMount(){
-        this[this.state.type]()
+        // this[this.state.type]()
         
     }
-    maps=()=>{ //地图
-        echarts.registerMap('xiant', scenemap);
+    maps=(scenegraph,cameracorrd)=>{ //地图
+        const cameralist=[];
+          cameracorrd.map((el,i)=>{
+            if(el.lat && el.lng){
+                    cameralist.push({value:[el.lng,el.lat],name:el.name})
+                }
+            })  
+        
+        if(scenegraph.indexOf('1000020')>0){
+            echarts.registerMap('xiant', apg);
+        }else{
+            echarts.registerMap('xiant', mqwl);
+        }
+        
         let option={
             background:"#091e57",
             geo: {
@@ -60,78 +75,7 @@ class Scenedata extends Component {
                     name: 'light',
                     type: 'scatter',
                     coordinateSystem: 'geo',
-                    data: [
-                            {name:'',
-                                value:[109.02390003204346,34.17467198883092],
-                                symbol:'image://https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548927397272&di=d988a5c1ff9ecd5785406657a15e3587&imgtype=0&src=http%3A%2F%2Fimg011.hc360.cn%2Fm2%2FM06%2F79%2FC0%2FwKhQcVRABumEB1GZAAAAABycr1Q784.jpg..700x700.jpg'
-                            },
-                            {name:'',
-                                value:[109.02390003204346,34.17229311310708],
-                                symbol:'image://https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548927397272&di=d988a5c1ff9ecd5785406657a15e3587&imgtype=0&src=http%3A%2F%2Fimg011.hc360.cn%2Fm2%2FM06%2F79%2FC0%2FwKhQcVRABumEB1GZAAAAABycr1Q784.jpg..700x700.jpg'
-                            },
-                        {name:'',
-                            value:[109.02390003204346,34.17467198883092],
-                            symbol:'image://https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548927397272&di=d988a5c1ff9ecd5785406657a15e3587&imgtype=0&src=http%3A%2F%2Fimg011.hc360.cn%2Fm2%2FM06%2F79%2FC0%2FwKhQcVRABumEB1GZAAAAABycr1Q784.jpg..700x700.jpg'
-                        },
-                        {name:'',
-                            value:[109.02390003204346,34.16998518451599],
-                            symbol:'image://https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548927397272&di=d988a5c1ff9ecd5785406657a15e3587&imgtype=0&src=http%3A%2F%2Fimg011.hc360.cn%2Fm2%2FM06%2F79%2FC0%2FwKhQcVRABumEB1GZAAAAABycr1Q784.jpg..700x700.jpg'
-                        },
-                        {name:'',
-                            value:[109.02390003204346,34.16760617668499],
-                            symbol:'image://https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548927397272&di=d988a5c1ff9ecd5785406657a15e3587&imgtype=0&src=http%3A%2F%2Fimg011.hc360.cn%2Fm2%2FM06%2F79%2FC0%2FwKhQcVRABumEB1GZAAAAABycr1Q784.jpg..700x700.jpg'
-                        },
-
-                        {name:'',
-                            value:[109.0239429473877,34.16526261087611],
-                            symbol:'image://https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548927397272&di=d988a5c1ff9ecd5785406657a15e3587&imgtype=0&src=http%3A%2F%2Fimg011.hc360.cn%2Fm2%2FM06%2F79%2FC0%2FwKhQcVRABumEB1GZAAAAABycr1Q784.jpg..700x700.jpg'
-                        },
-                        {name:'',
-                            value:[109.0239429473877,34.16295449004889],
-                            symbol:'image://https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548927397272&di=d988a5c1ff9ecd5785406657a15e3587&imgtype=0&src=http%3A%2F%2Fimg011.hc360.cn%2Fm2%2FM06%2F79%2FC0%2FwKhQcVRABumEB1GZAAAAABycr1Q784.jpg..700x700.jpg'
-                        },
-                        {name:'',
-                            value:[109.0239429473877,34.160646306117926],
-                            symbol:'image://https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548927397272&di=d988a5c1ff9ecd5785406657a15e3587&imgtype=0&src=http%3A%2F%2Fimg011.hc360.cn%2Fm2%2FM06%2F79%2FC0%2FwKhQcVRABumEB1GZAAAAABycr1Q784.jpg..700x700.jpg'
-                        },
-                        {name:'',
-                            value:[109.0239429473877,34.158302547099595],
-                            symbol:'image://https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548927397272&di=d988a5c1ff9ecd5785406657a15e3587&imgtype=0&src=http%3A%2F%2Fimg011.hc360.cn%2Fm2%2FM06%2F79%2FC0%2FwKhQcVRABumEB1GZAAAAABycr1Q784.jpg..700x700.jpg'
-                        },
-                        {name:'',
-                            value:[109.0239429473877,34.15599423599647],
-                            symbol:'image://https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548927397272&di=d988a5c1ff9ecd5785406657a15e3587&imgtype=0&src=http%3A%2F%2Fimg011.hc360.cn%2Fm2%2FM06%2F79%2FC0%2FwKhQcVRABumEB1GZAAAAABycr1Q784.jpg..700x700.jpg'
-                        },
-                        {name:'',
-                            value:[109.01188373565674,34.17463648371606],
-                            symbol:'image://https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548927397272&di=d988a5c1ff9ecd5785406657a15e3587&imgtype=0&src=http%3A%2F%2Fimg011.hc360.cn%2Fm2%2FM06%2F79%2FC0%2FwKhQcVRABumEB1GZAAAAABycr1Q784.jpg..700x700.jpg'
-                        },
-                        {name:'',
-                            value:[109.01188373565674,34.17002069158771],
-                            symbol:'image://https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548927397272&di=d988a5c1ff9ecd5785406657a15e3587&imgtype=0&src=http%3A%2F%2Fimg011.hc360.cn%2Fm2%2FM06%2F79%2FC0%2FwKhQcVRABumEB1GZAAAAABycr1Q784.jpg..700x700.jpg'
-                        },
-                        {name:'',
-                            value:[109.01188373565674,34.167677192815034],
-                            symbol:'image://https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548927397272&di=d988a5c1ff9ecd5785406657a15e3587&imgtype=0&src=http%3A%2F%2Fimg011.hc360.cn%2Fm2%2FM06%2F79%2FC0%2FwKhQcVRABumEB1GZAAAAABycr1Q784.jpg..700x700.jpg'
-                        },
-                        {name:'',
-                            value:[109.01192665100098,34.1653691380062],
-                            symbol:'image://https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548927397272&di=d988a5c1ff9ecd5785406657a15e3587&imgtype=0&src=http%3A%2F%2Fimg011.hc360.cn%2Fm2%2FM06%2F79%2FC0%2FwKhQcVRABumEB1GZAAAAABycr1Q784.jpg..700x700.jpg'
-                        },
-                        {name:'',
-                            value:[109.01192665100098,34.16306102009149],
-                            symbol:'image://https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548927397272&di=d988a5c1ff9ecd5785406657a15e3587&imgtype=0&src=http%3A%2F%2Fimg011.hc360.cn%2Fm2%2FM06%2F79%2FC0%2FwKhQcVRABumEB1GZAAAAABycr1Q784.jpg..700x700.jpg'
-                        },
-                        {name:'',
-                            value:[109.01192665100098,34.16071732810289],
-                            symbol:'image://https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548927397272&di=d988a5c1ff9ecd5785406657a15e3587&imgtype=0&src=http%3A%2F%2Fimg011.hc360.cn%2Fm2%2FM06%2F79%2FC0%2FwKhQcVRABumEB1GZAAAAABycr1Q784.jpg..700x700.jpg'
-                        },
-                        {name:'',
-                            value:[109.01201248168945,34.156136287732515],
-                            symbol:'image://https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548927397272&di=d988a5c1ff9ecd5785406657a15e3587&imgtype=0&src=http%3A%2F%2Fimg011.hc360.cn%2Fm2%2FM06%2F79%2FC0%2FwKhQcVRABumEB1GZAAAAABycr1Q784.jpg..700x700.jpg'
-                        }
-
-                     ],
+                    data: cameralist,
                     symbolSize: 18, //圈圈大小
                     label: {
                         normal: {
@@ -148,51 +92,14 @@ class Scenedata extends Component {
                             color: '#f4258e'
                         }
                     }
-                },
-                {
-                    type:"effectScatter",// series图表类型
-                    coordinateSystem:"geo",// series坐标系类型
-                    // data:datasMap, // series数据内容
-                    effectType:"ripple", //涟漪特效
-                    itemStyle: {
-                        normal: {
-                            color: '#f4e925', //圈圈的颜色 
-                            shadowBlur: 10,
-                            shadowColor: '#333'
-                        }
-                    },
-                    showEffectOn: 'render',
-                    rippleEffect: {
-                        brushType: 'stroke',
-                        scale: 3, //设置缩放
-                        period: 2, //设置时间
-                    },
-                    hoverAnimation: true,
-                    label: {
-                        normal: {
-                            formatter: '{b}',
-                            position: 'right',
-                            show: false
-                        },
-                        emphasis: {
-                            show: false,
-                            // color: '#3F5874', //hover时字的颜色
-                        }
-                    },
-                    symbolSize:20 //涟漪大小
                 }
             ]
         }
-        this.setState({option})
+        return option;
     }
-    componentWillReceiveProps(nextProps){
-        if(nextProps!==this.state.father){
-            this.setState({
-                father:nextProps
-            },()=>{                
-                this.componentDidMount()
-            })
-        }
+    shouldComponentUpdate(nextProps,nextstate){
+        console.log('nextProps',nextProps)
+        return true;
     }
     onByModelClick = (e)=>{
         if(e.componentType === "series"){
@@ -205,12 +112,18 @@ class Scenedata extends Component {
     }
     render() {
         const _this=this;
+        console.log('this.props.scenegraph',this.props.scenegraph)
         return (
-            <ReactEcharts
-                option={this.state.option}
+            <div>
+                {this.props.scenegraph
+                    ?<ReactEcharts
+                option={this.maps(this.props.scenegraph,this.props.cameracorrd)}
                 onEvents={this.onClickByModel}
                 style={{height:'450px'}}
             />
+            :''}
+            </div>
+            
         )
     }
 }
