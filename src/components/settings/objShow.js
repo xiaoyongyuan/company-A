942bb4927b1fbc12537f17aaf3ad3de824627419
xiaoyongyuan <2207294253@qq.com>
2019-04-17 14:20:30 +0800

    围界

--- src/components/settings/objShow.js
+++ src/components/settings/objShow.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
-
-
-
 class objShow extends Component {
     constructor(props){
         super(props);
     }
-
-
-
-
+    componentDidMount() {
+        var c=document.getElementById("myCanvas");
+        var ctx=c.getContext("2d");
+        ctx.lineWidth=1;
+        ctx.strokeStyle="#C61717";
+        ctx.beginPath();
+        ctx.moveTo(30,30);
+        ctx.lineTo(150,30);
+        ctx.lineTo(150,150);
+        ctx.lineTo(30,150);
+        ctx.lineTo(30,30);
+        ctx.stroke();
+    }
     render() {
+        let objShow={
 
+        };
+        let labelStyle={
+            display:"inlineBlock",
+            width:"40px",
+            height:"20px",
+            color:"#fff"
+        };
         return (
-           <div>
-                sss
+           <div style={{objShow}}>
+               <span style={{labelStyle}}>车</span>
+               <canvas id="myCanvas" width="550px" height="500px" style={{background:"#A0A0A0"}} />
            </div> 
         )
     }
