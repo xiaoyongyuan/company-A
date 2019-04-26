d7a5ae46a6ca6a961eb6ef5f91d601671980b0e0
azj7319 <568963700@qq.com>
2019-04-26 17:03:21 +0800

    11

--- src/components/live/index.js
+++ src/components/live/index.js
@@ -1,53 +1,49 @@
 import React from 'react';
-import VideoJsForReact from 'videojs-for-react';
-// import 'videojs/dist/video-js.min.css';
-// import videojs from '../../utils/video/video.js';
+import videojs from 'video.js';
+import 'video.js/dist/video-js.min.css';
+import videoswf from '../../utils/video/video-js.swf';
 
-class Live extends React.Component {
-		constructor() {
-      super()
-      this.state = {
-        videoJsOptions: {
-          preload: 'auto', // 预加载
-          bigPlayButton: {}, // 大按钮
-          autoplay: true,// 自动播放
-          controls: true,// 是否开启控制栏
-          width: 800,// 播放器宽度
-          height: 600,// 播放器高度
-          // teachOrder:['flash'],
-          playbackRates: [1, 1.5, 2], // 播放倍速
-          sources: [// 视频源
-            {
-              src: 'http://yunxianchang.live.ujne7.com/vod-system-bj/44_176_20170224113626af3a75cd-3508-4bc3-b51f-366fca3c7e39.m3u8',
-              type: 'application/x-mpegURL',
-              label: '资源1',
-              withCredentials: false,
-              res: 960
-            },{
-              src: 'http://www.w3school.com.cn/i/movie.mp4',
-              type: 'video/mp4',
-              label: '资源3',
-              res: 1080
-            }
-          ]
-        }
-      }
+export default class Live extends React.Component {
+  componentWillMount(){
+    
+  }
+  componentDidMount() {
+    // videojs.options.flash.swf = videoswf;
+    this.player = videojs('myvideo', {
+        preload: 'auto',  // 预加载
+        bigPlayButton: {},  // 大按钮
+        autoplay: true,   // 自动播放
+        controls: true,  // 是否开启控制栏
+        width: 800,   // 播放器宽度
+        height: 600,  // 播放器高度
+        playbackRates: [1, 1.5, 2], 
+        muted: true, //是否循环播放
+        loop : true, //是否静音
+        autoplay:true, //是否自动播放     
+    }, function onPlayerReady() {
+      this.src({
+        src: 'rtmp://58.200.131.2:1935/livetv/hunantv',
+        type:'rtmp/flv'
+        // src: zonglan,
+        // type:'video/mp4'
+      })
+    });
+    
+  }
+
+  componentWillUnmount() {
+    if (this.player) {
+      this.player.dispose()
     }
-    // rtmp://mobliestream.c3tv.com:554/live/goodtv.sdp
-    // "rtmp://192.168.1.19/live/stream7"
-    // http://pic01.aokecloud.cn/alarm/1000011/video/20181229/1000011_20181229095336.mp4
-    // http://www.w3school.com.cn/i/movie.mp4
-    render() {
-        return (
-            <div className="live" style={{height: '100%', overflow: 'hidden'}}>
-              <VideoJsForReact
-                sourceChanged={(player) => console.log(player)}
-                onReady={(player) => console.log('准备完毕', player)}
-                {...this.state.videoJsOptions}
-                />
-            </div>
-            )
-    }
+  }
+  render() {
+    return (
+      <div>    
+        <div data-vjs-player>
+          <video ref={ node => this.videoNode = node } className="video-js" id="myvideo"></video>
+        </div>
+        
+      </div>
+    )
+  }
 }
-
-export default Live;
